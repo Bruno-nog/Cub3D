6a3408a9d505d4f4488b720f8e492994dafe8c54
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-10-01 18:51:45 -0300

    added struct.h

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,55 +6,16 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 15:21:37 by ratanaka          #+#    #+#             */
-/*   Updated: 2025/10/01 18:42:34 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/10/01 18:51:15 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <stdbool.h>
-
+# include "struct.h"
 # include "../minilibx-linux/mlx.h"
 # include "../libft/libft.h"
-
-typedef struct s_player
-{
-	float	x;
-	float	y;
-	float	angle;
-
-	bool	key_up;
-	bool	key_down;
-	bool	key_left;
-	bool	key_right;
-
-	bool	left_rotate;
-	bool	right_rotate;
-}	t_player;
-
-typedef struct s_game
-{
-	void		*mlx;
-	void		*win;
-	void		*img;
-
-	char		*data;
-	int			bpp;
-	int			size_line;
-	int			endian;
-
-	char		**map;
-	t_player	player;
-
-}	t_game;
-
-typedef struct s_global
-{
-	float	x1;
-	char	**map;
-	t_game	game;
-}	t_global;
 
 # define W 119
 # define A 97
@@ -71,22 +32,22 @@ typedef struct s_global
 # define DEBUG 0
 
 t_global	*gg(void);
-void	init_player(t_player *player);
-void	move_player(t_player *player);
-int		key_press(int keycode, t_player *player);
-int		key_release(int keycode, t_player *player);
-char 	**read_map(const char *path);
-int		exit_game(t_game *game);
-void	init_game(t_game *game, char *av);
+void		init_player(t_player *player);
+void		move_player(t_player *player);
+int			key_press(int keycode, t_player *player);
+int			key_release(int keycode, t_player *player);
+char 		**read_map(const char *path);
+int			exit_game(t_game *game);
+void		init_game(t_game *game, char *av);
 
-void	draw_square(int x, int y, int size, int color);
-void	draw_map(t_game *game);
-void	draw_vision(t_game *game, float ray_x, float ray_y, int i);
-void	draw_line(t_player *player, t_game *game, float start_x, int i);
-void	clear_image(t_game *game);
-void	put_pixel(int x, int y, int color, t_game *game);
-float	fixed_dist(float y1, float x2, float y2, t_game *game);
-bool	touch(float px, float py, t_game *game);
+void		draw_square(int x, int y, int size, int color);
+void		draw_map(t_game *game);
+void		draw_vision(t_game *game, float ray_x, float ray_y, int i);
+void		draw_line(t_player *player, t_game *game, float start_x, int i);
+void		clear_image(t_game *game);
+void		put_pixel(int x, int y, int color, t_game *game);
+float		fixed_dist(float y1, float x2, float y2, t_game *game);
+bool		touch(float px, float py, t_game *game);
 
 // GRAPHIC
 
