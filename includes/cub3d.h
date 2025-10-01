bdf4165ae916714d90a9fec07666b51e859bea77
Zephele <rafaeltanakax@gmail.com>
2025-10-01 11:11:00 -0300

    raycasting_incomplete

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,20 +3,90 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ratanaka <ratanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 15:21:37 by ratanaka          #+#    #+#             */
-/*   Updated: 2025/09/29 15:59:30 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/10/01 10:40:53 by ratanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <math.h>
+
 # include "../minilibx-linux/mlx.h"
 # include "../libft/libft.h"
 
+typedef struct s_player
+{
+	float	x;
+	float	y;
+	float	angle;
+
+	bool	key_up;
+	bool	key_down;
+	bool	key_left;
+	bool	key_right;
+
+	bool	left_rotate;
+	bool	right_rotate;
+}	t_player;
+
 typedef struct s_game
+{
+	void		*mlx;
+	void		*win;
+	void		*img;
+
+	char		*data;
+	int			bpp;
+	int			size_line;
+	int			endian;
+
+	char		**map;
+	t_player	player;
+
+}	t_game;
+
+# define W 119
+# define A 97
+# define S 115
+# define D 100
+# define LEFT 65361
+# define RIGHT 65363
+
+# define PI 3.14159265359
+
+# define WIDTH 1280
+# define HEIGHT 720
+# define BLOCK 64
+
+void	init_player(t_player *player);
+void	move_player(t_player *player);
+int		key_press(int keycode, t_player *player);
+int		key_release(int keycode, t_player *player);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+typedef struct s_game1
 {
 	void	*mlx;
 	void	*win;
@@ -44,7 +114,7 @@ typedef struct s_game
 	int		y_player;
 	int		moves;
 	int		endgame;
-}	t_game;
+}	t_game1;
 
 # define KEY_ESC 65307
 # define KEY_Q 113
@@ -70,7 +140,7 @@ int		render_map(t_game *game);
 void	game_start(t_game *game);
 
 //GAME
-void	move_player(t_game *game, int dx, int dy, char direction);
+// void	move_player(t_game *game, int dx, int dy, char direction);
 int		keypress(int keycode, t_game *game);
 void	gameplay(t_game *game);
 void	put_moves(t_game *game);
--- srcs/main.c
+++ srcs/main.c
@@ -3,219 +3,155 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ratanaka <ratanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/29 15:26:44 by brunogue          #+#    #+#             */
-/*   Updated: 2025/09/29 15:26:44 by brunogue         ###   ########.fr       */
+/*   Created: 2025/09/30 17:27:26 by ratanaka          #+#    #+#             */
+/*   Updated: 2025/10/01 10:58:35 by ratanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-#include <stdlib.h>
-#include <stdio.h>
-#include "mlx.h"
-
-int exit_game(t_game *game)
+void	put_pixel(int x, int y, int color, t_game *game)
 {
-    if (!game)
-        exit(0);
-    if (game->mlx && game->win)
-    {
-        mlx_destroy_window(game->mlx, game->win);
-        game->win = NULL;
-    }
-    free(game);
-    exit(0);
-    return (0);
+	int	index;
+
+	if (x >= WIDTH || y >= HEIGHT || x < 0 || y < 0)
+		return ;
+	index = y * game->size_line + x * game->bpp / 8;
+	game->data[index] = color & 0xFF;
+	game->data[index + 1] = (color >> 8) & 0xFF;
+	game->data[index + 2] = (color >> 16) & 0xFF;
 }
 
-int keypress(int keycode, t_game *game)
+void	draw_square(int x, int y, int size, int color, t_game *game)
 {
-    if (keycode == KEY_ESC || keycode == KEY_Q)
-        exit_game(game);
-    return (0);
+	int	i;
+
+	i = 0;
+	while (i++ < size)
+		put_pixel(x + i, y, color, game);
+	i = 0;
+	while (i++ < size)
+		put_pixel(x, y + i, color, game);
+	i = 0;
+	while (i++ < size)
+		put_pixel(x + size, y + i, color, game);
+	i = 0;
+	while (i++ < size)
+		put_pixel(x + i, y + size, color, game);
 }
 
-int keyrelease(int keycode, t_game *game)
+void	draw_map(t_game *game)
 {
-    (void)keycode;
-    (void)game;
-    return (0);
+	char	**map;
+	int		color;
+	int		x;
+	int		y;
+
+	map = game->map;
+	color = 0x000FF;
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			if (map[y][x] == '1')
+				draw_square(x * 64, y * 64, 64, color, game);
+			x++;
+		}
+		y++;
+	}
 }
 
-int render_frame(t_game *game)
+void	clear_image(t_game *game)
 {
-    (void)game;
-    return (0);
+	int	y;
+	int	x;
+
+	y = 0;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			put_pixel(x, y, 0, game);
+			x++;
+		}
+		y++;
+	}
 }
 
-void game_start(t_game *game)
+char	**get_map(void)
 {
-    game->mlx = mlx_init();
-    if (!game->mlx)
-    {
-        perror("Erro: mlx_init falhou");
-        free(game);
-        exit(1);
-    }
-    game->win = mlx_new_window(game->mlx, 1000, 1000, "Cub3D");
-    if (!game->win)
-    {
-        perror("Erro: mlx_new_window falhou");
-        free(game);
-        exit(1);
-    }
-    game->endgame = 0;
-    game->moves = 1;
+	char **map = malloc(sizeof(char *) * 11);
+
+	map[0] = "111111111111111";
+	map[1] = "100000000000001";
+	map[2] = "100000000000001";
+	map[3] = "100000100000001";
+	map[4] = "100000000000001";
+	map[5] = "100000010000001";
+	map[6] = "100001000000001";
+	map[7] = "100000000000001";
+	map[8] = "100000000000001";
+	map[9] = "111111111111111";
+	map[10] = NULL;
+	return (map);
 }
 
-int main(void)
+void	init_game(t_game *game)
 {
-    t_game  *game;
-
-    game = malloc(sizeof(t_game));
-    if (!game)
-    {
-        perror("Erro na alocação de memória para game");
-        return (1);
-    }
-    game->mlx = NULL;
-    game->win = NULL;
-    game_start(game);
-    mlx_hook(game->win, 2, 1L << 0, keypress, game);
-    mlx_hook(game->win, 3, 1L << 1, keyrelease, game);
-    mlx_hook(game->win, 17, 0L, (int (*)(void *))exit_game, game);
-    mlx_loop_hook(game->mlx, render_frame, game);
-    mlx_loop(game->mlx);
-    return (0);
+	game->mlx = mlx_init();
+	// colocar minha inplemenatacoa
+	game->map = get_map();
+	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "Game");
+	game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+	game->data = mlx_get_data_addr(game->img, &game->bpp,
+			&game->size_line, &game->endian);
+	mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
 }
 
+bool	touch(float px, float py, t_game *game)
+{
+	int	x;
+	int	y;
 
-// int exit_game(t_game *game)
-// {
-//     if (game && game->mlx)
-//         free(game->mlx);
-//     if (game)
-//         free(game);
-//     exit (0);
-//     return (0);
-// }
+	x = px / BLOCK;
+	y = py / BLOCK;
+}
 
-// int	keypress(int keycode, t_game *game)
-// {
-// 	if (keycode == KEY_ESC || keycode == KEY_Q)
-// 		exit_game(game);
-// 	return (0);
-// }
+int	draw_loop(t_game *game)
+{
+	t_player	*player;
+	float		ray_x;
+	float		ray_y;
+	float		cos_angle;
+	float		sin_angle;
 
-// void	game_start(t_game *game)
-// {
-// 	game->mlx = mlx_init();
-// 	game->win = mlx_new_window(game->mlx, 1000, 1000, "Cub3D");
-// 	game->endgame = 0;
-// 	game->moves = 1;
-// }
+	player = &game->player;
+	move_player(player);
+	clear_image(game);
+	draw_square(player->x, player->y, 10, 0x00FF00, game);
+	draw_map(game);
+	mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
 
-// int main()
-// {
-//     t_game  *game;
+	ray_x = player->x;
+	ray_y = player->y;
+	cos_angle = cos(player->angle);
+	sin_angle = sin(player->angle);
+	return (0);
+}
 
-//     game = (t_game *)malloc(sizeof(t_game));
-//     if (!game)
-//     {
-//         perror("Erro na alocação de memória para game");
-//         return (1);
-//     }
-//     game->mlx = NULL;
-//     game->win = NULL;
-//     game_start(game);
-//     if (game->win)
-//     {
-//         mlx_hook(game->win, 2, 1L << 0, keypress, game);
-//         mlx_hook(game->win, 17, 1L << 17, exit_game, game);
-//         mlx_loop(game->mlx);
-//     }
-//     else
-//         exit_game(game); 
-//     return (0);
-// }
+int	main(void)
+{
+	t_game	game;
 
-
-// #include <stdio.h>
-// #include "../includes/so_long.h"
-
-// // static void	size_window_start(t_game *game)
-// // {
-// // 	int	i;
-
-// // 	game->map_w = ft_strlen(game->map[0]) * 32;
-// // 	i = 0;
-// // 	while (game->map[i])
-// // 		i++;
-// // 	game->map_h = i * 32;
-// // }
-// static void	free_mlx(t_game *game)
-// {
-// 	mlx_destroy_display(game->mlx);
-// 	free(game->mlx);
-// }
-
-
-// int	exit_game(t_game *game)
-// {
-// 	// if (game->map)
-// 	// 	free_map(game->map);
-// 	// if (game->mapcopy)
-// 	// 	free_map(game->mapcopy);
-// 	// if (game->mapcopy2)
-// 	// 	free_map(game->mapcopy2);
-// 	// if (game->img_backgroud)
-// 	// 	mlx_destroy_image(game->mlx, game->img_backgroud);
-// 	// if (game->img_wall)
-// 	// 	mlx_destroy_image(game->mlx, game->img_wall);
-// 	// if (game->img_player)
-// 	// 	mlx_destroy_image(game->mlx, game->img_player);
-// 	// if (game->img_colect)
-// 	// 	mlx_destroy_image(game->mlx, game->img_colect);
-// 	// if (game->img_exit)
-// 	// 	mlx_destroy_image(game->mlx, game->img_exit);
-// 	// if (game->img_victory)
-// 	// 	mlx_destroy_image(game->mlx, game->img_victory);
-// 	// if (game->win)
-// 	// 	mlx_destroy_window(game->mlx, game->win);
-// 	if (game->mlx)
-// 		free_mlx(game);
-// 	exit (0);
-// 	return (0);
-// }
-
-// int	keypress(int keycode, t_game *game)
-// {
-// 	if (keycode == KEY_ESC || keycode == KEY_Q)
-// 		exit_game(game);
-// 	return (0);
-// }
-
-// void	game_start(t_game *game)
-// {
-// 	game->mlx = mlx_init();
-// 	// size_window_start(game);
-// 	game->win = mlx_new_window(game->mlx, 1000, 1000, "so_long");
-// 	// put_moves(game);
-// 	game->endgame = 0;
-// 	game->moves = 1;
-// 	// start_image(game);
-// 	// render_map(game);
-// }
-
-// int main ()
-// {
-// 	t_game	*game;
-// 	game = NULL;
-
-// 	game_start(game);
-// 	mlx_hook(game->win, 2, 1L << 0, keypress, game);
-// 	mlx_hook(game->win, 17, 1L << 17, exit_game, game);
-// 	mlx_loop(game->mlx);
-// }
+	init_game(&game);
+	mlx_hook(game.win, 2, 1L<<0, key_press, &game.player);
+	mlx_hook(game.win, 3, 1L<<1, key_release, &game.player);
+	mlx_loop_hook(game.mlx, draw_loop, &game);
+	mlx_loop(game.mlx);
+	return (0);
+}
